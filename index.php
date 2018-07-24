5379b9c76e172b89d5dccce407fe5b09a75821ee
Dan Plows <plowdk51@gmail.com>
2018-07-23 20:24:40 -0400

    added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Memo Trainer</title>
+<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+
+<?php
+echo "<h1>Memo Trainer</h1>";
+?>
+
+</body>
+</html>
